52f678f4fea34d2837665087be086847f2b70d94
Almaz <3112almaz@gmail.com>
2023-04-25 22:12:09 +0300

    tic_tac_toe

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import logging
-
 from telegram import ReplyKeyboardMarkup
-from telegram.ext import Application, MessageHandler, filters, ConversationHandler
+from telegram.ext import Application, MessageHandler, filters
 from telegram.ext import CommandHandler
 
-from config import BOT_TOKEN
 from tic_tac_toe import tic_tac_toe, check_end_of_tic_tac_toe, board
-from wordle import wordle, wordle_answer, wordle_difficulty
+from random import randint
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
@@ -14,7 +12,7 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-start_keyboard = [['/tic_tac_toe Крестики нолики', '/wordle Wordle']]
+start_keyboard = [['/tic_tac_toe Крестики нолики']]
 start_markup = ReplyKeyboardMarkup(start_keyboard, one_time_keyboard=True)
 
 
@@ -25,52 +23,67 @@ async def start(update, context):
 
 async def mini_games(update, context):
     text = update.message.text
-    if 'game' not in context.user_data:
-        return
     if context.user_data["game"][0] == "tic_tac_toe":
         field = context.user_data["game"][1]
-        tic_tac_toe_field_keyboard = [[f"/1_1 {field[0][0]}", f"/1_2 {field[0][1]}", f"/1_3 {field[0][2]}"],
-                                      [f"/2_1 {field[1][0]}", f"/2_2 {field[1][1]}", f"/2_3 {field[1][2]}"],
-                                      [f"/3_1 {field[2][0]}", f"/3_2 {field[2][1]}", f"/3_3 {field[2][2]}"]]
-        tic_tac_toe_markup = ReplyKeyboardMarkup(tic_tac_toe_field_keyboard, one_time_keyboard=True)
         try:
             text = text[1:4].split("_")
             if field[int(text[0]) - 1][int(text[1]) - 1] == "  ":
                 field[int(text[0]) - 1][int(text[1]) - 1] = "x"
             else:
                 await update.message.reply_text("Данное поле уже занято")
-            await update.message.reply_text(f"|{field[0][0]}|{field[0][1]}|{field[0][2]}|\n"
-                                            f"|{field[1][0]}|{field[1][1]}|{field[1][2]}|\n"
-                                            f"|{field[2][0]}|{field[2][1]}|{field[2][2]}|\n",
-                                            reply_markup=tic_tac_toe_markup)
+                return 0
         except Exception:
             await update.message.reply_text("Вы ввели поле в неправильном формате или поле уже занято")
-        await update.message.reply_text("Ваш ход")
-        if check_end_of_tic_tac_toe(field):
-            await update.message.reply_text("Вы Победили")
-        board(field)
+            return 0
+        tic_tac_toe_field_keyboard = [[f"/1_1 {field[0][0]}", f"/1_2 {field[0][1]}", f"/1_3 {field[0][2]}"],
+                                      [f"/2_1 {field[1][0]}", f"/2_2 {field[1][1]}", f"/2_3 {field[1][2]}"],
+                                      [f"/3_1 {field[2][0]}", f"/3_2 {field[2][1]}", f"/3_3 {field[2][2]}"]]
+        tic_tac_toe_markup = ReplyKeyboardMarkup(tic_tac_toe_field_keyboard,
+                                                 one_time_keyboard=True)
+        await update.message.reply_text("Ваш ход",
+                                        reply_markup=tic_tac_toe_markup)
+        board(field, update.message.from_user)
         await context.bot.send_photo(
             update.message.chat_id,
-            "res.png"
+            f"{update.message.from_user}.png"
         )
+        if check_end_of_tic_tac_toe(field):
+            await update.message.reply_text("Вы победили")
+            await tic_tac_toe(update, context)
+            return 0
+        draw = 0
+        for i in range(len(field)):
+            for j in range(len(field)):
+                if field[i][j] == "  ":
+                    draw += 1
+        if draw == 0:
+            await update.message.reply_text("Ничья")
+            await tic_tac_toe(update, context)
+            return 0
+        Ai_tic_tac_toe = []
+        for i in range(len(field)):
+            for j in range(len(field)):
+                if field[i][j] == "  ":
+                    Ai_tic_tac_toe.append(i * 3 + j)
+        s = Ai_tic_tac_toe[randint(0, len(Ai_tic_tac_toe) - 1)]
+        field[s // 3][s % 3] = "o"
+        await update.message.reply_text("Ход бота",
+                                        reply_markup=tic_tac_toe_markup)
+        board(field, update.message.from_user)
+        await context.bot.send_photo(
+            update.message.chat_id,
+            f"{update.message.from_user}.png"
+        )
+        if check_end_of_tic_tac_toe(field):
+            await update.message.reply_text("Бот победил")
+            await tic_tac_toe(update, context)
 
 
 def main():
-    application = Application.builder().token(BOT_TOKEN).build()
+    application = Application.builder().token("6183870254:AAH2HrtJcJaeD_3wQBso2WeFrTWZTlvNja4").build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("tic_tac_toe", tic_tac_toe))
-    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, mini_games)
-
-    application.add_handler(ConversationHandler(
-        entry_points=[CommandHandler('wordle', wordle)],
-
-        states={
-            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, wordle_difficulty)],
-            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, wordle_answer)]
-        },
-        fallbacks=[]
-    ))
-
+    text_handler = MessageHandler(filters.TEXT, mini_games)
     application.add_handler(text_handler)
     application.run_polling()
 
